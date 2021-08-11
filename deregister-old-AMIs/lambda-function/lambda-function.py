8580ef33124b84a06b46975dd30aa43027459fd3
vivek22117 <vivekmishra22117@gmail.com>
2021-08-11 11:13:07 +0530

    updating lambda function

--- deregister-old-AMIs/lambda-function/lambda-function.py
+++ deregister-old-AMIs/lambda-function/lambda-function.py
@@ -1,6 +1,10 @@
 import datetime
 import boto3
 from dateutil.parser import parse
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 
 def days_old(date):
@@ -11,23 +15,35 @@ def days_old(date):
 
 def lambda_handler(event, context):
 
-    # Get list of regions
-    ec2_client = boto3.client('ec2')
-    regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
+    try:
 
-    for region in regions:
-        ec2 = boto3.client('ec2', region_name=region)
-        print('Region: ', region)
+        # Get list of regions
+        ec2_client = boto3.client('ec2')
+        regions = [region['RegionName'] for region in ec2_client.describe_regions()['Regions']]
 
-        all_ami = ec2.describe_images(Owners=['self'])['Images']
+        for region in regions:
+            ec2 = boto3.client('ec2', region_name=region)
+            print('Region: ', region)
 
-        for ami in all_ami:
-            creation_date = ami['CreationDate']
-            age_days = days_old(creation_date)
-            image_id = ami['ImageId']
-            print('ImageId: {}, CreationDate: {} ({} days old)'.format(image_id, creation_date, age_days))
+            all_ami = ec2.describe_images(Owners=['self'])['Images']
 
-            if age_days >= 5:
-                print('Deleting imageId: ', image_id)
+            for ami in all_ami:
+                creation_date = ami['CreationDate']
+                age_days = days_old(creation_date)
+                image_id = ami['ImageId']
+                print('ImageId: {}, CreationDate: {} ({} days old)'.format(image_id, creation_date, age_days))
 
-                ec2.deregister_image(ImageId=image_id)
+                if age_days >= 5:
+                    print('Deleting imageId: ', image_id)
+                    ec2.deregister_image(ImageId=image_id)
+                    for ebs in ami['BlockDeviceMappings']:
+                        log.debug(ebs)
+                        if not (ebs.get('Ebs') is None):
+                            ec2_client.delete_snapshot(Shapshot=ebs['Ebs']['SnapshotId'])
+                            log.debug('Deleted...')
+                else:
+                    log.debug(ami['ImageId'] + " is not older than 5 days")
+
+    except Exception as ex:
+        log.error("Error occurred " + str(ex))
+        raise Exception(ex)
