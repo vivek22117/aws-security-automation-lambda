9c061d2838c86d60996156a44e1bc45be35e1237
vivek22117 <vivekmishra22117@gmail.com>
2021-08-17 14:18:26 +0530

    adding lambda for cloudtrail security

---
+++ handle-cloudtrail-start-stop/data.tf
@@ -0,0 +1,9 @@
+data "terraform_remote_state" "s3_buckets" {
+  backend = "s3"
+
+  config = {
+    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    key     = "state/${var.environment}/s3-buckets/terraform.tfstate"
+    region  = var.default_region
+  }
+}
