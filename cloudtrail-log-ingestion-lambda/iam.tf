359020893d0b797fae651c7ac4e1c06b7d449987
vivek22117 <vivekmishra22117@gmail.com>
2022-06-18 20:56:07 +0530

    adding IAM policy and role for lambda

---
+++ cloudtrail-log-ingestion-lambda/iam.tf
@@ -0,0 +1,63 @@
+resource "aws_iam_role" "cloudtrail_monitoring_lambda_role" {
+  name = var.lambda_role
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "cloudtrail_monitoring_lambda_policy" {
+  name        = var.lambda_policy
+  description = "Policy to have delete access"
+  path        = "/"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:GetObjectVersion",
+      ],
+      "Resource": "${local.s3_arn}"
+    },
+   {
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource":[
+        ${local.s3_arn}",
+        "${local.s3_arn}/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "policy_role_attach" {
+  policy_arn = aws_iam_policy.cloudtrail_monitoring_lambda_policy.arn
+  role       = aws_iam_role.cloudtrail_monitoring_lambda_role.name
+}
+
