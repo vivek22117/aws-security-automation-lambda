94cf1e8257d4242fcb02efe6eb5f839913ce0995
vivek22117 <vivekmishra22117@gmail.com>
2022-06-18 20:55:47 +0530

    adding TF data resource

---
+++ cloudtrail-log-ingestion-lambda/data.tf
@@ -0,0 +1,20 @@
+###################################################
+#          Fetch TF remote state of VPC           #
+###################################################
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/s3-buckets/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+locals {
+  s3_arn  = data.terraform_remote_state.s3.outputs.cloudtrail_s3_arn
+  s3_name = data.terraform_remote_state.s3.outputs.cloudtrail_s3_name
+}
+
+
+data "aws_caller_identity" "current" {}
