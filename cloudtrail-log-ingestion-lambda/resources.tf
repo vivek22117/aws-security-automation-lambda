34b86ae76233030052f5c36d94be035d55e356c8
vivek22117 <vivekmishra22117@gmail.com>
2022-06-18 22:05:53 +0530

    adding lambda for cloudtrail security

--- cloudtrail-log-ingestion-lambda/resources.tf
+++ cloudtrail-log-ingestion-lambda/resources.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_notification" "aws-lambda-trigger" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.cloudtrail_monitoring_lambda.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix = "AWSLogs/"
+    filter_prefix = "audit/AWSLogs/"
   }
 }
 
